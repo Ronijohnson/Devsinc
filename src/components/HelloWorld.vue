381d53633888e9802f17cd7020474e88d3e23386
Ronnie_johnson11-22 <ronijohn1122@gmail.com>
2023-03-29 13:03:55 +0500

    First Commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,317 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+  <div class="main-container">
+
+  <div class="nav-bar">
+    <div> <img class="logo-for-resonsive" src="../assets/logooo.png" alt=""></div>
+    <div class="hamburger"><img src="../assets/menu.png" alt=""></div>
+  <div class="nav-for-responsive">
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li><a>Home</a> </li>
+      <li><a>Offering</a> </li>
+      <li><a>Industries</a> </li>
+      <li><a>Partners</a> </li>
+      <li><a>Testimonials</a></li>
+      <li><a>Insights</a> </li>
+      <li><a>Company</a> </li>
+      <li><a>Contact Us</a></li>
     </ul>
   </div>
+  </div>
+ <div> <H1 class="first-h1">How we tranform business to unlock value</H1> </div>
+  <div class="start-button-style"><button class="button-1">Start Now</button></div>
+  
+</div>
+<!-- second section start from here -->
+<div class="about-section">
+  <div class="about">
+    <div><h1 class="about-us ">About us</h1></div>
+    <div> 
+      <p class="about-paragraph">New Vision Softcom & Consultancy, is a hard and smart working IT Services Company, that excels in growing businesses
+       of Small and Medium companies to Enterprise levels, by way of solving Business pain areas with sound technological 
+       solutions. We pride ourselves of having delivered successful projects on .NET, Java, 
+      Data Analytics, IOT & ERP solutions for almost every vertical locally and globally.....
+</p>
+     <div class="responsive-button">
+
+      <button class="responvie-button-style">Try For Free</button>
+
+     </div>
+    </div>
+  </div>
+  <div><img class="responsive-imag-style" src="../assets/men.png" alt=""></div>
+
+</div>
+
+
+<!-- offering section start..... -->
+
+<div class="offering-section">
+  <div><h1>Offering</h1></div>
+  <div><h3 style="font-size: 20px;">Services we offer</h3></div>
+  <div class="offering-box">
+   
+    
+      <div class="oracal-box oracal-mobile-box-responsive">
+      
+      <div>
+        <hr class="hr-oracal Hr-line-of-all-boxex-responsive">
+        <img class="mobile-responsive-img" src="../assets/img.png" alt=""></div>
+      <div><p class="oracal-heading">Oracle Utilies</p></div>
+      <div><p class="oracal-tex">Customer to Meter(C2M) <span>,Customer Care and Bolling(CC&B), Meter Data Management(MDM),
+        Field Service(OFS),Ananlytics(OUA)</span></p></div>
+
+    </div>
+    <div class="cloud-box  oracal-mobile-box-responsive">
+      
+      <div>
+        <hr class="hr-cloud Hr-line-of-all-boxex-responsive">
+        <img class="mobile-responsive-img" src="../assets/cloud.png" alt=""></div>
+      <div><p class="cloud-heading">Cloud</p></div>
+      <div><p class="oracal-tex">Microsoft Azure, AWS </p></div>
+
+    </div>
+    
+    
+      <div class="bussniess-box oracal-mobile-box-responsive">
+      
+      <div>
+        <hr class="hr-bussniess Hr-line-of-all-boxex-responsive">
+        <img class="mobile-responsive-img" src="../assets/bussiness.png" alt=""></div>
+      <div><p class="bussniess-heading">Business Process Management</p></div>
+      <div><p class="bussnies-tex">Salesforce, PEGA, Robomagic & UI Path </p></div>
+
+    </div>
+    <div class="Enterprise-box  oracal-mobile-box-responsive">
+      
+      <div>
+        <hr class="hr-Enterprise  Hr-line-of-all-boxex-responsive">
+        <img class="mobile-responsive-img"  src="../assets/Enterprise.png" alt=""></div>
+      <div><p class="bussniess-heading">Enterprise Resource Planning</p></div>
+      <div><p class="bussnies-tex">Orcale EBS, JD Edwards, NetSuite, ODOO, SAP </p></div>
+
+    </div>
+    
+    
+  </div>
+  <div><button class="view-all-button"> <p class="view-all-button-heading">View all</p></button></div>
+
+</div>
+
+<!-- .............partnership section.......... -->
+
+<div class="partner-session">
+  <di><h1 class="partershit-heading">Partners</h1></di>
+  <di><p class="partnership-text">At NewVision, strategic partnesrships are one of the core pillars of our business strategy.
+    We have a 360 degree relationship with our partners and our approach is aimed at delivering unparalledvalue to our clients. Both NewVision and
+     the partners share common set of cultureal values and 
+     drive common business onjectives</p></di>
+  <di class="partner-web-img"><img class="partnershit-img-style" src="../assets/partnership.png" alt=""></di>
+  <div class="res-img"> <img src="../assets//mobileresponsive.png" alt=""></div>
+  <div class="res-img"><img src="../assets//mobile.png.png" alt=""></div>
+
+</div>
+
+
+<!-- ..........Testimonials Section.......... -->
+
+<div class="Testimonials-Section">
+  <div><h1>Testimonials</h1></div>
+  <div><p>Hear it from the Industry Leaders</p></div>
+  <div class="tesimonials-box-main-section">
+    <div class="testimonial-first-box">
+
+<div style="display: flex; flex-direction: column;align-items: center;">
+
+<div class="testimonials-starts">
+  <div><img class="collon"  src="../assets/collon.png" alt=""></div>
+  <div><img src="../assets/star.png" alt=""></div>
+  <div><img src="../assets/star.png" alt=""></div>
+  <div><img src="../assets/star.png" alt=""></div>
+  <div><img src="../assets/star.png" alt=""></div>
+  <div><img src="../assets/star.png" alt=""></div>
+</div>
+<div class="testimonial-text"><p>NewVision provided great service, appropriate architecture and prompt response to issue at a reasonable rate. 
+Their team was professional and always had a can-do attitude despite our changing needs and timelines. I hughly
+ recommend giving them a try whether you have a POC or major application solution project on your roadmap.</p></div>
+ 
+ <div class="testimonial-bottom">
+  <div class="Testiomonial-img-margon">
+  <img src="../assets//Testimonial01img.png" alt="">
+ </div>
+
+  <div><p class="tesimonial-button-heading">CIO, U.S.Based Corporate Wellness Company</p>
+</div>
+ </div>
+   </div>
+
+
+</div>
+<div class="testimonial-first-box-first">
+
+<div style="display: flex; flex-direction: column;align-items: center;">
+
+<div class="testimonials-starts">
+  <div><img class="collon"  src="../assets/collon.png" alt=""></div>
+  <div><img src="../assets/star.png" alt=""></div>
+  <div><img src="../assets/star.png" alt=""></div>
+  <div><img src="../assets/star.png" alt=""></div>
+  <div><img src="../assets/star.png" alt=""></div>
+  <div><img src="../assets/star.png" alt=""></div>
+</div>
+<div class="testimonial-text"><p>I would like to take my hats off for one of the best tean that I have worked with.
+   Asim Noise, Anmol, and their team members were exceptional in supporting , analyzing , and delivering our project, from start to end. Their work has made a significant different to our business.
+   I hope them the best and will definitely work with them in the future as need be.</p></div>
+ 
+ <div class="testimonial-bottom">
+  <div class="Testiomonial-img-margon">
+  <img src="../assets//second.png" alt="">
+ </div>
+
+  <div><p class="tesimonial-button-heading">COO, The Baker, Saudi Arabia</p>
+</div>
+ </div>
+   </div>
+
+
+</div>
+<div class="testimonial-first-box">
+
+<div style="display: flex; flex-direction: column;align-items: center;">
+
+<div class="testimonials-starts">
+  <div><img class="collon" src="../assets/collon.png" alt=""></div>
+  <div><img src="../assets/star.png" alt=""></div>
+  <div><img src="../assets/star.png" alt=""></div>
+  <div><img src="../assets/star.png" alt=""></div>
+  <div><img src="../assets/star.png" alt=""></div>
+  <div><img src="../assets/star.png" alt=""></div>
+</div>
+<div class="testimonial-text"><p>NewVision has done an excellent job in SAP implementation and integration with our POS. 
+  We really see value from our investements and the partner</p></div>
+ 
+ <div class="testimonial-bottom">
+  <div class="Testiomonial-img-margon">
+  <img src="../assets//third.png" alt="">
+ </div>
+
+  <div><p class="tesimonial-button-heading">CEO, Rajgreen Group</p>
+</div>
+ </div>
+   </div>
+
+
+</div>
+
+  </div>
+
+  <div style="margin-top: 3rem;"><button class="Testiomonials-button">More testimonias</button></div>
+  
+
+</div>
+
+
+
+<!-- ....................BLOCK-SESSION....... -->
+
+
+
+<div class="block-section">
+  <div style="padding-bottom: 5rem; "> <h1 class="blok-h1">Blogs</h1></div>
+  <div class="block-two-box-styl">
+
+    <div class="block-box-first-box">
+    <div><img class="for-mobile-responsive" src="../assets//blogpic.png" alt=""></div>
+    <div class="block-box-bottom   for-mobile-view-also">
+
+ <div class="button-time-day">
+  <div><p class="Day-heading">On 12 Aug, 2020</p></div>
+  <div><p class="time-heading">6 min read</p></div>
+
+ </div>
+ <div>
+  <p class="last-blog-heading">5 Signs that Your Enterprise Apps Need to be Upgraded and Modernized</p>
+ </div>
+
+ <div><button class="read-more-button">Read more</button></div>
+ <div class="hr"></div>
+
+    </div>
+  </div>
+  <div class="block-box-first-box  roni">
+    <div><img src="../assets//Rectangle.png" alt=""></div>
+    <div class="block-box-bottom">
+
+ <div class="button-time-day">
+  <div><p class="Day-heading">On 12 Aug, 2020</p></div>
+  <div><p class="time-heading">6 min read</p></div>
+
+ </div>
+ <div>
+  <p class="last-blog-heading">5 Signs that Your Enterprise Apps Need to be Upgraded and Modernized</p>
+ </div>
+
+ <div><button class="read-more-button">Read more</button></div>
+ <div style="background: #FAD606;" class="hr"></div>
+
+    </div>
+  </div>
+
+
+  </div>
+  <div> <button class="show-all-button"> <p class="show-all-text">Show all >></p></button></div>
+</div>
+
+  <!-- ............................contact-us-section.................... -->
+
+
+  <div class="contact-us-section">
+  <div class="contact-us-headig">
+    <div><h1 class="contact-for-mobile-responsive-style">Contact Us</h1></div>
+    <div><p class="contus-us-second-hading">Questions ? We will put you on the right path.</p></div>
+  </div>
+    <div class="map-section">
+   
+      <div class="contact-us-fileds">
+        <div><p class="for-mob-resp-contact">contact us </p></div>
+        <input class="input-name" type="text" name="" id="" placeholder="Your Name">
+        <input class="input-name" type="text" name="" id="" placeholder="Your Email">
+       <span> <input class="your-message" type="text" name="" id="" placeholder="Your Message"></span>
+        <div><button class="send-button"><p class="send-button-text">Send</p></button></div>
+      </div>
+      <div class="map-section-icon">
+        <div class="icon-row">
+          <div class="for-mobile-responsive-location">
+            <img class="location-img-style-for-mobile" src="../assets//Vector.png" alt="">
+          <p class="location-icon-text-style">602, Pentagon Tower 1, Magarpatla City, Hadapsar, Pune India</p>
+          </div>
+          <div class="for-mobile for-web">
+            <div><img src="../assets//vectorone.png" alt="">
+          <p class="phone-icon-text">+91 8956001836</p>
+          </div>
+          <div><img src="../assets//vectortow.png" alt="">
+          <p>contact@newvisionsoftware.in</p>
+          </div>
+          </div>
+        </div>
+        <div><img class="img-style-map" src="../assets//map.png" alt=""></div>
+        <div class="last-twiter-icon-row">
+          <div><img src="../assets//vectorthree.png" alt=""></div>
+        <div><img src="../assets//vfour.png" alt=""></div>
+        <div><img src="../assets//vfive.png" alt=""></div>
+        </div>
+        
+      </div>
+
+    </div>
+   <div class="bottom-footer">
+   <div><p style="margin-right: 3rem;">Copyright</p></div>
+   <div><p>2021 - New Vision Softcom & Consultancy Pvt. Ltd</p></div>
+  </div>
+  </div>
+
+
+
+  
 </template>
 
 <script>
@@ -38,21 +322,7 @@ export default {
   }
 }
 </script>
+<style src="./Helloworld.css" scoped></style>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+
+
